051d62cdd33718eccfd19c53490bebc172a0826d
Nattawat Khawman <6010110185@psu.ac.th>
2022-08-17 18:56:21 +0700

    2

--- card-game/src/App.js
+++ card-game/src/App.js
@@ -1,23 +1,29 @@
 import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
+import WordCard from './WordCard';
+const sad=()=>{
+  return {s:0};
+}
 
 function App() {
+  const [state, setState] = useState(sad())
+  const z=state.s==1? "correct":state.s==2?"Incorrect":""
+  const check=value=>{
+    setState({s:1})
+  }
+  const f=()=>{
+    setState({s:2})
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <br></br>
+       <center><div>รูปภาพนี้คือมังงะเรื่องอะไร?</div></center><br></br>
+      <center><img src ="https://sololevelingthailand.com/wp-content/uploads/2021/04/Jogo-e-projeto-live-action-de-Solo-Leveling-1.jpg" width="280px" height="350px"/></center>
+      <center><div className="card" onClick={f}>One Piece</div></center>
+      <center><div className="card" onClick={f}>Naruto Shippuden</div></center>
+      <center><div className="card" onClick={check}>Solo leveling</div></center><br></br>
+      <center><div className="card activeCard">{z}</div></center>
     </div>
   );
 }
